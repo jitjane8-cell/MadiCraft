180d315fbb1c04b7c46888a12ba7dfc0f7b6649d
jitjane8@gmail.com <jitjane8@gmail.com>
2026-05-12 17:21:25 +0700

    first website

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black text-white flex flex-col items-center justify-center">
+
+      <h1 className="text-6xl font-bold text-cyan-400">
+        STAR NETWORK
+      </h1>
+
+      <p className="mt-4 text-gray-300 text-xl">
+        เซิร์ฟเวอร์ Minecraft สุดมันส์
+      </p>
+
+      <button className="mt-6 bg-cyan-500 px-6 py-3 rounded-2xl hover:bg-cyan-400 transition">
+        play.star-network.com
+      </button>
+
+    </main>
+  )
+}
